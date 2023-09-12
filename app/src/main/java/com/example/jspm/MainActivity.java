43111b8e3b4010f9bf0cc3bb64156ae0db1427c0
Shubham <shubhamchoureps@gmail.com>
2023-09-12 20:19:47 +0530

    Firebase Firestore Added

--- app/src/main/java/com/example/jspm/MainActivity.java
+++ app/src/main/java/com/example/jspm/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.jspm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +21,5 @@ EditText editText;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        editText.findViewById(R.id.shubham);
-        editText.setText("hahahahahaah");
     }
 }
