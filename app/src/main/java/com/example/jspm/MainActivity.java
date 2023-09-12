e84ea2a6f395db6c8325d01374ac87b5d5eb482f
omkar <omkarjagtap318@gmail.com>
2023-09-12 19:35:54 +0530

    login page xml

--- app/src/main/java/com/example/jspm/MainActivity.java
+++ app/src/main/java/com/example/jspm/MainActivity.java
@@ -7,13 +7,9 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        editText.findViewById(R.id.shubham);
-        editText.setText("hahahahahaah");
     }
 }
