7508e18907ce0b35ac4dec5793e7b5dc827a6c07
Shubham <shubhamchoureps@gmail.com>
2023-09-16 15:11:28 +0530

    Firebase Firestore Added

--- app/src/main/java/com/example/jspm/MainActivity.java
+++ app/src/main/java/com/example/jspm/MainActivity.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
