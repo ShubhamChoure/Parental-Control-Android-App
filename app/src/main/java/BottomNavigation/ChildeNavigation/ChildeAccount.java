edce7ce9ee30b16b03b495ea87e39eda004b3c76
Shubham Choure <shubhamchoureps@gmail.com>
2024-02-10 00:55:34 +0530

    App is done , splash screen pending

--- app/src/main/java/BottomNavigation/ChildeNavigation/ChildeAccount.java
+++ app/src/main/java/BottomNavigation/ChildeNavigation/ChildeAccount.java
@@ -1,13 +1,17 @@
 package BottomNavigation.ChildeNavigation;
 
+import static HomeActivity.ChildeHomeActivity.ChildHomeActivity.mAuth;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,7 +22,13 @@ import android.widget.Toast;
 
 import com.example.jspm.MainActivity;
 import com.example.jspm.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.io.ByteArrayInputStream;
 
 import HomeActivity.ChildeHomeActivity.ChildHomeActivity;
 
@@ -89,11 +99,12 @@ public class ChildeAccount extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         init();
-
+        anandi(); //For setting user details from dataabse
+        shubham(); //For setting child name and mail address
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ChildHomeActivity.mAuth.signOut();
+                mAuth.signOut();
                 Intent mainIntent = new Intent(ChildHomeActivity.MyContext, MainActivity.class);
                 getActivity().startActivity(mainIntent);
                 ((Activity)ChildHomeActivity.MyContext).finish();
@@ -101,17 +112,71 @@ public class ChildeAccount extends Fragment {
         });
     }
 
+    void anandi() {
+        //For setting user details from dataabse
+
+        db.collection("User").whereEqualTo("Email",mAuth.getCurrentUser().getEmail().toString()).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                acNameTV.setText(task.getResult().getDocuments().get(0).getString("Name"));
+                mailAddressTV.setText(task.getResult().getDocuments().get(0).getString("Email"));
+                mobileNoTV.setText(task.getResult().getDocuments().get(0).getString("Phone No"));
+                bloodGroupTV.setText(task.getResult().getDocuments().get(0).getString("Blood Type"));
+
+                String base64Encoded = task.getResult().getDocuments().get(0).getString("uri");
+                byte[] byteArray = Base64.decode(base64Encoded,Base64.DEFAULT);
+
+                Drawable imgDrawable = byteArrayToDrawable(byteArray, "Profile Pic");
+                if(imgDrawable!=null){
+                    profilePicIV.setImageDrawable(imgDrawable);
+                }
+            }
+        });
+
+    }
+
+    void shubham(){
+        //For setting child name and mail address
+        db.collection("Relation").whereEqualTo("LinkChild",mAuth.getCurrentUser().getEmail().toString()).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+
+                childMailAddressTV.setText(task.getResult().getDocuments().get(0).getString("Mail"));
+                setChildName();
+
+            }
+        });
+    }
+
+    void setChildName() {
+        db.collection("User").whereEqualTo("Email",childMailAddressTV.getText().toString().trim()).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                for(QueryDocumentSnapshot document:task.getResult()){
+                    childNameTV.setText(document.getString("Name"));
+                }
+            }
+        });
+    }
+
+
     void init()
     {
         logoutBtn = getView().findViewById(R.id.ChildFragmentLogoutBtn);
         db = FirebaseFirestore.getInstance();
 
-        acNameTV = getView().findViewById(R.id.parentACName);
-        mailAddressTV = getView().findViewById(R.id.mailParentACTV);
-        mobileNoTV = getView().findViewById(R.id.mobileNoParentACTV);
-        bloodGroupTV = getView().findViewById(R.id.bloodParentACTV);
-        childMailAddressTV = getView().findViewById(R.id.childMailParentACTV);
-        childNameTV = getView().findViewById(R.id.childNameParentACTV);
-        profilePicIV = getView().findViewById(R.id.profilePicParentIV);
+        acNameTV = getView().findViewById(R.id.childACName);
+        mailAddressTV = getView().findViewById(R.id.mailChildACTV);
+        mobileNoTV = getView().findViewById(R.id.mobileNoChildACTV);
+        bloodGroupTV = getView().findViewById(R.id.bloodChildACTV);
+        childMailAddressTV = getView().findViewById(R.id.parentMailChildACTV);
+        childNameTV = getView().findViewById(R.id.parentNameChildACTV);
+        profilePicIV = getView().findViewById(R.id.profilePicChildIV);
+    }
+    Drawable byteArrayToDrawable(byte[] bar, String appName) {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bar);
+        Drawable drawable = Drawable.createFromStream(byteArrayInputStream, appName);
+        return drawable;
     }
 }
